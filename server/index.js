d4aa64540699846a7f323bbc2815b1b4cc4b185d
amaan35 <amaanlm35@gmail.com>
2024-02-21 04:34:38 +0530

    creating and instantiating our socket server, listening for connection and disconnect events

--- server/index.js
+++ server/index.js
@@ -2,10 +2,27 @@ const express = require('express');
 const app = express()
 const http = require('http');
 const cors = require('cors');
+//Importing a class named server from socketIO lib
+const { Server } = require('socket.io'); 
 
 app.use(cors());
 
 const server = http.createServer(app);
+
+//Instantiating our socket IO server
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on("connection", (socket)=>{
+    console.log("socket id : ", socket.id);
+    socket.on("disconnect", ()=>{
+        console.log("User disconnected : ", socket.id);
+    })
+})
 
 server.listen(3001, ()=>{
     console.log('server started...')
